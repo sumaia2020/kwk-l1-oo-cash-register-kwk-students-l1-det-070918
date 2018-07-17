04ce844c1fbfebd2ce264e23f2a80e68fdb130fd
sumaia2020 <sbalghaiti@hawkmail.hfcc.edu>
2018-07-17 13:09:21 +0000

    automatically backed up by learn

--- amazon.rb
+++ amazon.rb
@@ -1,7 +1,7 @@
 
 
-amazon_hash = ["Shoes" =>40, "Laptop" => 500, "Water bottle" => 20, "Album" => 25, "Headphones" => 30]
-amazon_hash.each do |item_name, price|
+# amazon_hash = ["Shoes" =>40, "Laptop" => 500, "Water bottle" => 20, "Album" => 25, "Headphones" => 30]
+# amazon_hash.each do |item_name, price|
   
 end
 puts "Please select your items:"
@@ -15,21 +15,22 @@ counter = 0
 answer = gets.chomp
 while answer == "Shoes, shoes"
     counter += 40
+  end
   while answer == "Laptop, laptop"
     counter += 500
+  end
   while answer == "Water bottle, Water Bottle, water bottle"
   counter += 20
+end
   while answer == "Album , album"
   counter += 25
+end
   while answer == "Headphones, headphones"
   counter += 30
+end
  puts counter
 end 
-end
- end 
- end 
- end 
- end
+
   
 
   
